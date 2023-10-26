e2d22411bd15e6c201e93a7004d85bfe42cf07d3
Taalaibek Sagynaliev <sagynalievv.t@gmail.com>
2023-10-27 00:21:46 +0600

    webinar 53

--- src/App.tsx
+++ src/App.tsx
@@ -3,80 +3,116 @@ import './App.css';
 import React, {useState} from "react";
 
 interface Person {
-    id: number;
-    name: string;
-    age: number;
-    hobby: string;
+  id: number;
+  name: string;
+  age: number;
+  hobby: string;
 }
 
 function App() {
-    const [people, setPeople] = useState<Person[]>([
-        {name: 'Jane', age: 28, hobby: 'Video games', id: 1},
-        {name: 'John', age: 30, hobby: 'Football', id: 2},
-        {name: 'Jack', age: 69, hobby: 'Drinking alone', id: 3},
-        {name: 'Dima', age: 34, hobby: 'Coding', id: 4},
-    ]);
+  const [people, setPeople] = useState<Person[]>([
+    {name: 'Jane', age: 28, hobby: 'Video games', id: 1},
+    {name: 'John', age: 30, hobby: 'Football', id: 2},
+    {name: 'Jack', age: 69, hobby: 'Drinking alone', id: 3},
+    {name: 'Dima', age: 34, hobby: 'Coding', id: 4},
+  ]);
 
-    const [showPeople, setShowPeople] = useState(false);
+  const [showPeople, setShowPeople] = useState(false);
 
-    const changeName = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
-        const peopleCopy = [...people];
-        const person = peopleCopy[index];
-        const personCopy = {...person};
-        personCopy.name = event.target.value;
-        peopleCopy[index] = personCopy;
-        setPeople(peopleCopy);
-    };
+  // const changeName = (event: React.ChangeEvent<HTMLInputElement>, id: number) => {
+  //   const index = people.findIndex((person) => person.id === id);
+  //   const peopleCopy = [...people];
+  //   const person = peopleCopy[index];
+  //   const personCopy = {...person};
+  //   personCopy.name = event.target.value;
+  //   peopleCopy[index] = personCopy;
+  //   setPeople(peopleCopy);
+  // };
 
-    const changeAge = () => {
-        const peopleCopy = people.map(person => {
-            return {...person, age: person.age + 1};
-        });
+  const changeName = (event: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    const newName = event.target.value;
 
-        setPeople(peopleCopy);
-    };
+    setPeople((prevState) => prevState.map((person) => {
+      if (person.id === id) {
+        return {...person, name: newName};
+      }
 
-    const increaseAge = (index: number) => {
-        const peopleCopy = [...people];
-        const person = peopleCopy[index];
-        const personCopy = {...person};
-        personCopy.age++;
-        peopleCopy[index] = personCopy;
-        setPeople(peopleCopy);
-    };
+      return person;
+    }));
+  };
 
-    /*Мы берем prevState и меняем его*/
-    const togglePeople = () => {
-        setShowPeople((prevState) => !prevState);
-    };
+  const changeAge = () => {
+    setPeople((prevState) => prevState.map(person => {
+      return {...person, age: person.age + 1};
+    }));
+  };
 
-    let peopleList: React.ReactNode = null;
+  const increaseAge = (id: number) => {
+    setPeople((prevState) => prevState.map((person) => {
+      if (person.id === id) {
+        return {...person, age: person.age + 1};
+      }
 
-    if (showPeople) {
-        peopleList = people.map((person, index) => {
-            return (
-                <Person
-                    key={person.id}
-                    name={person.name}
-                    age={person.age}
-                    onNameClick={() => increaseAge(index)}
-                    onNameChange={(event) => changeName(event, index)}
-                >
-                    <strong>Hobby: </strong>{person.hobby}
-                </Person>
-            );
-        });
-    }
+      return person;
+    }));
+  };
 
-    return (
-        <div className="App">
-            {peopleList}
-            <div>
-                <button onClick={changeAge}>Change age</button>
-                <button onClick={togglePeople}>Toggle people</button>
-            </div>
-        </div>
-    )
+  // const removePerson = (id: number) => {
+  //   const index = people.findIndex((person) => person.id === id);
+  //   const peopleCopy = [...people];
+  //   peopleCopy.splice(index, 1);
+  //   setPeople(peopleCopy);
+  // };
+
+  const removePerson = (id: number) => {
+    setPeople((prevState) => prevState.filter(person => {
+      return person.id !== id;
+    }));
+  };
+
+
+  /*Мы берем prevState и меняем его*/
+  const togglePeople = () => {
+    setShowPeople((prevState) => !prevState);
+  };
+
+  const addPerson = () => {
+    setPeople((prevState) => {
+      const id = prevState[prevState.length - 1].id + 1;
+      console.log(prevState.length);
+      return [...prevState, {name: 'Someone', age: 21, hobby: 'Something', id}];
+    });
+  };
+
+  let peopleList: React.ReactNode = null;
+
+  if (showPeople) {
+    peopleList = people.map((person) => {
+      return (
+        <Person
+          key={person.id}
+          name={person.name}
+          age={person.age}
+          onNameClick={() => increaseAge(person.id)}
+          onNameChange={(event) => changeName(event, person.id)}
+          remove={() => removePerson(person.id)}
+        >
+          <strong>Hobby: </strong>{person.hobby}
+        </Person>
+      );
+    });
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={changeAge}>Change age</button>
+        <button onClick={togglePeople}>Toggle people</button>
+        <button onClick={addPerson}>Add people</button>
+      </div>
+      {peopleList}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 
 interface Props extends React.PropsWithChildren {
-    name: string;
-    age: number;
-    onNameClick: React.MouseEventHandler;
-    onNameChange?: React.ChangeEventHandler<HTMLInputElement>;
+  name: string;
+  age: number;
+  onNameClick: React.MouseEventHandler;
+  onNameChange?: React.ChangeEventHandler<HTMLInputElement>;
+  remove: React.MouseEventHandler;
 }
 
-const Person: React.FC<Props> = ({name, age, onNameClick, onNameChange, children}) => {
-    return (
-        <div className="person">
-            <h1 onClick={onNameClick}>{name}</h1>
-            <p>Age: {age}</p>
-            <p>{children}</p>
-            <input type="text" value={name} onChange={onNameChange}/>
-        </div>
-    )
+const Person: React.FC<Props> = ({
+  name, age,
+  onNameClick,
+  onNameChange,
+  remove,
+  children
+}) => {
+  return (
+    <div className="person">
+      <h1 onClick={onNameClick}>{name}</h1>
+      <p>Age: {age}</p>
+      <p>{children}</p>
+      <input type="text" value={name} onChange={onNameChange}/>
+      <button onClick={remove}>Remove</button>
+    </div>
+  );
 };
 
 export default Person;
